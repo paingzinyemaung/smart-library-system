4e54607b68a601ccdf9406f64e5cfd7c97f7ef02
PaingZinYeMaung <paingzinyemaung.pgo@gmail.com>
2026-09-09 11:44:49 +0630

    feat(app): auto-open browser on application ready

--- src/main/java/com/oodd/library/LibraryApplication.java
+++ src/main/java/com/oodd/library/LibraryApplication.java
@@ -1,7 +1,11 @@
 package com.oodd.library;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class LibraryApplication {
@@ -9,5 +13,14 @@ public class LibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
+
+	@EventListener(ApplicationReadyEvent.class)
+    public void openBrowser() {
+        try {
+            new ProcessBuilder("cmd", "/c", "start", "http://localhost:8585").start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
